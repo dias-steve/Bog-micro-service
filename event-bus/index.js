3e3fb90baef42953e296ca5509c7780a58fa9137
Steve Dias <stevediaspro@gmail.com>
2023-01-08 13:46:11 +0100

    end

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,24 +5,29 @@ const axios =require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+// setting the event stock manager
+// for wether a service doen't have all the events
+
+const events = [];
+
+
 app.post('/events',(req,res) => {
     const event = req.body;
 
+    events.push(event);
 
-        axios.post('http://localhost:4003/events', event).catch(err=>console.log(err.message));//query service
-        axios.post('http://localhost:4002/events', event).catch(err=>console.log(err.message));//posts service
-        axios.post('http://localhost:4001/events', event).catch(err=>console.log(err.message));//comments service
-        axios.post('http://localhost:4006/events', event).catch(err=>console.log(err.message));//moderation service
-      
+    axios.post('http://localhost:4003/events', event).catch(err=>console.log(err.message));//query service
+    axios.post('http://localhost:4002/events', event).catch(err=>console.log(err.message));//posts service
+    axios.post('http://localhost:4001/events', event).catch(err=>console.log(err.message));//comments service
+    axios.post('http://localhost:4006/events', event).catch(err=>console.log(err.message));//moderation service
 
-
-
-
-  
-
-
-
+    res.send({status: 'OK'});
 } );
+
+// get all events received
+app.get('/events', (req, res) => {
+    res.send(events);
+});
 
 app.listen(4005, () => {
     console.log('[event bus]Listening on 4005');
--- query/index.js
+++ query/index.js
@@ -1,6 +1,9 @@
+// this service reformat posts data: it combine all data related to posts such as comments ... 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios =require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,14 +14,9 @@ const posts = {
 
 };
 
-
-app.get('/posts', (req,res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const {type, data} = req.body;
-    console.log(req.body);
+const handleEvent = (type, data) => {
+   
+    
     if(type === 'PostCreated'){
         const { id, title } = data; 
 
@@ -46,11 +44,27 @@ app.post('/events', (req, res) => {
         comment.content = content;
     }
 
-    console.log(posts);
+}
+app.get('/posts', (req,res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const {type, data} = req.body;
+   
+    handleEvent(type, data);
 
     res.send({})
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
     console.log('[Query Service] Listening on 4003');
+
+    //Sync events: we get all events 
+    const res = await axios.get('http://localhost:4005/events').catch(err=>console.log(err.message));
+
+    for(let event of res.data){
+        console.log('Processing event:', event.type);
+        handleEvent(event.type, event.data);
+    }
 });
